73689435e1e226437a3d7f7a2d3f44df13f2b2e1
vanthinh97 <thinhdangvan97@gmail.com>
2022-03-23 08:45:01 +0700

    add userrepository

---
+++ newBusinessApp/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using newBusinessApp.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace newBusinessApp.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+    }
+}
